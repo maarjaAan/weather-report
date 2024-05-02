6b1a85fe9b03550155e64e2ee1ec1c3ea1f1ad46
maarjaAan <166254179+maarjaAan@users.noreply.github.com>
2024-05-02 16:08:09 +0200

    added basic forecast info to  index.js and also styled it

--- index.js
+++ index.js
@@ -44,4 +44,32 @@ function searchCity(city) {
 
 searchCity("Tallinn");
 
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+  
+    days.forEach(function (day) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>15º</strong>
+            </div>
+            <div class="weather-forecast-temperature">9º</div>
+          </div>
+        </div>
+      `;
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+}
+
+
+  
+
+  displayForecast();
 
