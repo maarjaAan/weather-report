f1f3269eb3213bc7d1d677dbbfbb687f64c309f8
maarjaAan <166254179+maarjaAan@users.noreply.github.com>
2024-05-02 22:22:50 +0200

    added  forecast and attaxed API with that

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function refreshWeather(response) {
     humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
     windElement.innerHTML=`${response.data.wind.speed}km/h`;
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
-}
+  
+    getForecast(response.data.city);
+  }
 
 function searchCity(city) {
     let apiKey="fd0f7619afd34b6f4ebdft43802e1ofb";
@@ -42,34 +44,50 @@ function searchCity(city) {
   let weatherDate=document.querySelector("#weather-day");
   weatherDate.innerHTML=`${day} ${hours}:${minutes}`;
 
-searchCity("Tallinn");
 
-function displayForecast() {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+
+function displayForecast(response) {
+ 
+
+   
     let forecastHtml = "";
   
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day,index) {
+      if(index <5){
       forecastHtml =
         forecastHtml +
         `<div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          <div class="weather-forecast-icon"><img src="${day.condition.icon_url}" class="weather-forecast-icon"/></div>
           <div class="weather-forecast-temperatures">
             <span class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
             </span>
-            <span class="weather-forecast-temperature-min">9º</span>
+            <span class="weather-forecast-temperature-min">${Math.round(day.temperature.minimum)}º</span>
           </div>
         </div>
       `;
+      }
     });
   
     let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
 }
 
+function getForecast(city){
+let  apiKey ="fd0f7619afd34b6f4ebdft43802e1ofb";
+let apitUrl=`https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+axios.get(apitUrl).then(displayForecast);
+
+}
+
+searchCity("Tallinn");
 
   
-
-  displayForecast();
 
